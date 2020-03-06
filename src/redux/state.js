9ffae6bb89e6e630dd1ce448b460c89f926e8218
tigooo <tigooo@mail.ru>
2020-03-06 13:43:33 +0400

    export redux

---
+++ src/redux/state.js
@@ -0,0 +1,37 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: "Hello how are you", likesCount: 11},
+            {id: 1, message: "Barev aper", likesCount: 25},
+            {id: 1, message: "Barev aper", likesCount: 55}
+        ]
+    },
+    dialogpage: {
+        dialogs: [
+            {id: 1, name: 'Tigo'},
+            {id: 2, name: 'Pash'},
+            {id: 3, name: 'Tehmina'},
+            {id: 4, name: 'Sevak'},
+            {id: 5, name: 'Gor'},
+            {id: 6, name: 'Arev'}
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How is your it-camasutra'},
+            {id: 3, message: 'Yo'},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Yo'},
+            {id: 6, message: 'Yo'}
+        ]
+    },
+    navbar: {
+        friends: [
+            {id: 1, user: 'Tigo', ava:'https://avatars.mds.yandex.net/get-pdb/1748372/2290e7d1-f820-4e85-aa0b-57c54492a9dc/s1200'},
+            {id: 2, user: 'Pash', ava:'https://pngimage.net/wp-content/uploads/2018/06/%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D0%B8-png-5.png'},
+            {id: 3, user: 'Ezo', ava:'https://pngimage.net/wp-content/uploads/2018/06/%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D0%B8-png-9.png'}
+        ]
+    }
+
+}
+
+export default state;
